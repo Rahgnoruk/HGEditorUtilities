e85ecf8c183d4b0e84d15cfd5d8d627a12d76dd4
Rahgnoruk <tono.raimond@hotmail.com>
2022-03-28 15:15:19 -0600

    Colorizer
    Sencillo asignador de colores para rapid prototyping

---
+++ Editor/Colorizer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+public class Colorizer : EditorWindow
+{
+    [MenuItem("Window/HyperGnosys/Colorizer")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow<Colorizer>("Colorizer");
+    }
+
+    Color color;
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Cambia el color del material de los GameObjects seleccionados", EditorStyles.boldLabel);
+        color = EditorGUILayout.ColorField("Nuevo Color", color);
+        if (GUILayout.Button("Colorize"))
+        {
+            Colorize(color);
+        }
+    }
+    private void Colorize(Color color)
+    {
+        foreach(GameObject obj in Selection.gameObjects)
+        {
+            Renderer renderer = obj.GetComponent<Renderer>();
+            if(renderer != null)
+            {
+                Undo.RecordObject(renderer.sharedMaterial, "Change Color");
+                renderer.sharedMaterial.color = color;
+            }
+        }
+    }
+}
